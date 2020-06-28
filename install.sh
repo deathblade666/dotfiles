896336b488946ef03ee5b1b439934d49099c5a83
deathblade666 <halo2ofdeath@gmail.com>
2020-06-28 10:31:36 -0400

    Added execution header

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 pacman -S --noconfirm ebtables fakeroot sudo dnsmasq archiso libvirt virt-manager git curl wget alsa-utils wpa_supplicant polkit-gnome zsh go base-devel htop nfs-utils vi vim hsetroot neofetch rofi i3-gaps xorg xorg-xinit qemu ovmf nvidia chromium qutebrowser steam ranger pcmanfm mpv <<< Y
 systemctl enable libvirtd
 mkdir /home/deathmasia/NAS
