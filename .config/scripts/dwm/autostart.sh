f78cb3ef4150790da833259280ae7757aa3cbd5a
deathblade666 <halo2ofdeath@gmail.com>
2020-09-19 18:19:26 -0400

    fixed incorrect syntax

--- .config/scripts/dwm/autostart.sh
+++ .config/scripts/dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xrandr -s 1920x1080 &
 exec hsetroot -full ~/Wallpapers/wallpaper.jpg &
-exe picom &
+exec picom &
 # Statusbar loop
 while true; do
    xsetroot -name "$( date +"%F %R" )"
