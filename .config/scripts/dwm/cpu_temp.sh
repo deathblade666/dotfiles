6c258344ba8390bfe8c39dc069983c1343198e98
deathblade666 <halo2ofdeath@gmail.com>
2020-11-15 13:41:21 -0500

    removing scripts that are not needed

--- .config/scripts/dwm/cpu_temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-CPU=$(sensors | sed -rn 's/.*Package id 0:\s+.([^ ]+).*/\1/p')
-
-# Full and short texts
-echo "CPU Temp: $CPU"
-
-exit 0
--- .config/scripts/dwm/gpu_temp.sh
+++
@@ -1,7 +0,0 @@
-#~/bin/#!/usr/bin/env bash
-GPU=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=temperature.gpu)
-
-# Full and short texts
-echo "GPU Temp: $GPU"
-
-exit 0
