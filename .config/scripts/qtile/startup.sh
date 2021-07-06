a318217b1d6140200fb75a57094e5ee38daf402d
deathblade666 <halo2ofdeath@gmail.com>
2021-07-06 19:47:30 -0400

    Updated Qtile startup script

--- .config/scripts/qtile/startup.sh
+++ .config/scripts/qtile/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 hsetroot -full $HOME/Pictures/wallpapers/scenery.jpg &
+barriers --no-tray --debug INFO --name ArchLinux --enable-crypto -c /home/deathmasia/.config/barrier/Barrier --address :24800 &
+picom &
 
 
